198363defcbadeb1a41e0f179030c6ab14463aa1
Charles Waldner <waldner.charles@gmail.com>
2019-08-20 10:27:24 -0600

    Add Edge

---
+++ SqlDependencyResolution/Edge.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqlDependencyResolution
+{
+    public class Edge
+    {
+        public int Id { get; set; }
+
+        public int? DependsOn { get; set; }
+    }
+}
